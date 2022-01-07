b2255fbc8627a3ffb59d6eb48a8d0775daa639a5
Aravind Wickey <aravind261998@gmail.com>
2022-01-07 11:55:36 +0530

    first commit react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+import RouterComponent from './RouterComponent'
+
+export default function App() {
+  return (
+    <><div>
+    <RouterComponent/>
+    </div>
+    </>
+  );
+}
