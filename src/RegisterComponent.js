0c5cdf6306dbc12672a89058c21b3a2eba791d4b
Aravind Wickey <aravind261998@gmail.com>
2022-01-16 08:29:08 +0530

    login component from register update

--- src/RegisterComponent.js
+++ src/RegisterComponent.js
@@ -2,11 +2,29 @@ import React, {useState} from 'react';
 import { Typography, TextField, Button } from '@mui/material'; 
 import axios from 'axios';
 import { typography } from "@mui/system";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    subtitle1: {
+      fontSize: 12,
+    },
+  },
+  Button: {
+    subtitle1: {
+      Width: 2,
+    },
+  },
+});
 
 function RegisterComponent(props) {
   const [name, setName] = useState("");
   const [mail, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const loginComponentFunction = async () => {
+    props.history.push("/login");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -62,13 +80,25 @@ function RegisterComponent(props) {
               type="password"
               placeholder="Password"
             />
+            <ThemeProvider theme={theme}>
+              <Typography variant="subtitle1">
+                If you're alredy user{" "}
+                <Button
+                  variant="text"
+                  sx={{ width: 2, ml: 0 }}
+                  onClick={loginComponentFunction}
+                >
+                  <Typography variant="subtitle1">LOGIN</Typography>
+                </Button>{" "}
+              </Typography>{" "}
+            </ThemeProvider>
             <br />
             <Button
               type="submit"
               value="Register"
               variant="contained"
               size="small"
-              sx={{ width: 2, mt: 2 }}
+              sx={{ width: 2, mt: -4 }}
             >
               Submit
             </Button>
